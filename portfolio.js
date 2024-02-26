676692f828a900fdb458aa2cabf56f9f51824fce
Adetoke Durojaiye <awesometokeh@gmail.com>
2024-02-26 16:44:06 +0100

    update

--- portfolio.js
+++ portfolio.js
@@ -1,31 +1,31 @@
-let roles = document.getElementById("rolecontainer");
-let jobRoles = ["Software Engineer", "Full Stack Developer", "Web Developer"];
-let index = 0;
-let charIndex = 0;
+// let roles = document.getElementById("rolecontainer");
+// let jobRoles = ["Software Engineer", "Full Stack Developer", "Web Developer"];
+// let index = 0;
+// let charIndex = 0;
 
-function displayJobRoles() {
-    const currentRole = jobRoles[index];
-    const displayedText = currentRole.substring(0, charIndex + 1);
-    roles.textContent = displayedText + (charIndex % 2 === 0 ? "|" : " ");
-    roles.style.opacity = 1;
+// function displayJobRoles() {
+//     const currentRole = jobRoles[index];
+//     const displayedText = currentRole.substring(0, charIndex + 1);
+//     roles.textContent = displayedText + (charIndex % 2 === 0 ? "|" : " ");
+//     roles.style.opacity = 1;
 
-    if (charIndex < currentRole.length) {
-        charIndex++;
-    } else {
-        setTimeout(() => {
-            roles.style.opacity = 0;
-            charIndex = 0;
+//     if (charIndex < currentRole.length) {
+//         charIndex++;
+//     } else {
+//         setTimeout(() => {
+//             roles.style.opacity = 0;
+//             charIndex = 0;
 
-            index = (index + 1) % jobRoles.length;
-        }, 1000);
-    }
-}
+//             index = (index + 1) % jobRoles.length;
+//         }, 1000);
+//     }
+// }
 
-displayJobRoles();
+// displayJobRoles();
 
-setInterval(displayJobRoles, 150);
+// setInterval(displayJobRoles, 150);
 
-document.getElementById('bar').addEventListener('click', function () {
-    document.querySelector('.info').classList.toggle('active');
-});
+// document.getElementById('bar').addEventListener('click', function () {
+//     document.querySelector('.info').classList.toggle('active');
+// });
 
